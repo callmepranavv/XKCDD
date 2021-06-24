ad592113f768a6e6d91764edd748e67ff8365c7f
ykram2019 <44055979+ykram2019@users.noreply.github.com>
2021-06-24 22:44:10 +0545

    Update mail.php

--- mail.php
+++ mail.php
@@ -3,9 +3,9 @@ session_start();
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
-require '\PHPMailer.php';
-require '\Exception.php';
-require '\SMTP.php';
+require '\libs\PHPMailer.php';
+require '\libs\Exception.php';
+require '\libs\SMTP.php';
 $mail = new PHPMailer(true);
 
 try {
