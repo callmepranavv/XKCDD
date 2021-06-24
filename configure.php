1321827603d61b96bd1e3ccaf649cf45d4296bda
ykram2019 <44055979+ykram2019@users.noreply.github.com>
2021-06-24 11:33:25 +0545

    second commit

--- configure.php
+++ configure.php
@@ -1,13 +1,8 @@
 <?php
-
-$con=mysqli_connect("localhost","root","","xcd");
+$con=mysqli_connect('us-cdbr-east-04.cleardb.com','bc4e5f17c88182','6c3ec0ea','heroku_8bd14e111ed2fa5');
 if(mysqli_connect_errno())
 {
     echo "Failed to connect to MYSQL:".mysqli_connect_errno();
     exit();
 }
-
-
-
-
 ?>
